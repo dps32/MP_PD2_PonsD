ec2f41a6e1c5dc19c91455aec29426998a496bb3
dps32 <munecodepruebas@gmail.com>
2025-10-09 18:54:15 +0200

    update: v0.1

--- www/js/index.js
+++ www/js/index.js
@@ -1,1 +1,11 @@
+$(document).ready( function ()  {
 
+    $('#inputBtn').click(addElem);
+
+
+    function addElem() {
+        const text = prompt("Nombre de la tarea:");
+        $('.tasklist .list').append(`<div class="task">${text}</div>`);
+    }
+
+});
