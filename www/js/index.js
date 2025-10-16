04a64422773bc329a9b9f15efdf648e63b5a2d0d
dps32 <munecodepruebas@gmail.com>
2025-10-16 19:13:59 +0200

    update: v0.2

--- www/js/index.js
+++ www/js/index.js
@@ -7,18 +7,36 @@ $(document).ready( function ()  {
 function addElem() {
     const text = prompt("Nombre de la tarea:");
     if (text) {
-        const elem = $(`<div class="task"><span>${text}</span><button class="delBtn">X</button></div>`);
+        const elem = $(`<div class="task"><input class='taskText' value='${text}'/><div><button class="editBtn">Edit</button><button class="saveBtn">Save</button><button class="delBtn">X</button></div></div>`);
         $('.tasklist .list').append(elem);
-        $('button', elem).click(delElem);
+        $('button.delBtn', elem).click(delElem);
+        $('button.editBtn', elem).click(editElem);
+        $('button.saveBtn', elem).click(saveElem);
     }
 }
 
 function delElem(e) {
     const eBtn = e.target ?? e.srcElement;
-    const task = eBtn.parentElement;
+    const task = $(eBtn.parentElement.parentElement); // otra vez porque ahora esta dentro del contenedor de btns
 
-    task.classList.add('hidden');
+    task.addClass('hidden');
     setTimeout(() => {
         task.remove();
     }, 300);
+}
+
+function editElem(e) {
+    const eBtn = e.target ?? e.srcElement;
+    const task = $(eBtn.parentElement.parentElement); // otra vez porque ahora esta dentro del contenedor de btns
+
+    const taskText = $('.taskText', task);
+    taskText.addClass('editable');
+}
+
+function saveElem(e) {
+    const eBtn = e.target ?? e.srcElement;
+    const task = $(eBtn.parentElement.parentElement); // otra vez porque ahora esta dentro del contenedor de btns
+
+    const taskText = $('.taskText', task);
+    taskText.removeClass('editable');
 }
