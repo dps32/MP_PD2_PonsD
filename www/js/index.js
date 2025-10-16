8efa55d4e62795c14363175fc85940a8ef37369a
dps32 <munecodepruebas@gmail.com>
2025-10-16 18:15:50 +0200

    update

--- www/js/index.js
+++ www/js/index.js
@@ -2,10 +2,20 @@ $(document).ready( function ()  {
 
     $('#inputBtn').click(addElem);
 
-
-    function addElem() {
-        const text = prompt("Nombre de la tarea:");
-        $('.tasklist .list').append(`<div class="task">${text}</div>`);
-    }
-
 });
+
+function addElem() {
+    const text = prompt("Nombre de la tarea:");
+    if (text) {
+        const elem = $(`<div class="task"><span>${text}</span><button>X</button></div>`);
+        $('.tasklist .list').append(elem);
+        $('button', elem).click(delElem);
+    }
+}
+
+function delElem(e) {
+    const eBtn = e.target ?? e.srcElement;
+    const task = eBtn.parentElement;
+
+    task.remove();
+}
